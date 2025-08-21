808e5aebee4c7041572bd22dc0b71f4f8df17794
Saratha1999 <ldsarathadevi1999@gmail.com'cleargit config --global user.email ldsarathadevi1999@gmail.com>
2025-08-21 17:09:47 -0500

    Update ALLOWED_HOSTS to use environment variable for Render deployment

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9pr+@7ez5-!(k)ay98x1d*z3tf6&uay9069gfto6-!sg9f$)zr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
