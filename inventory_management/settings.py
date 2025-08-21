a7a6c0e5d7d2035cf85dae6c69fcb78ab54d9b67
Saratha1999 <ldsarathadevi1999@gmail.com'cleargit config --global user.email ldsarathadevi1999@gmail.com>
2025-08-21 17:19:30 -0500

    Update ALLOWED_HOSTS to use environment variable for Render deployment

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9pr+@7ez5-!(k)ay98x1d*z3tf6&uay9069gfto6-!sg9f$)zr'
+#SECRET_KEY = 'django-insecure-9pr+@7ez5-!(k)ay98x1d*z3tf6&uay9069gfto6-!sg9f$)zr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-fallback-key")
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
